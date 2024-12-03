08376c57d3ea65ec384393947c50c01f48c156af
Steven Hunt <sph@commandersoft.co.uk>
2024-12-03 12:34:30 +0000

    d3 rs - Rust: only a mother could love it

--- rs/src/days/day03.rs
+++ rs/src/days/day03.rs
@@ -7,14 +7,34 @@ pub fn run() {
     let input = get_text_from_file(&filename).unwrap();
 
     let pattern1 = r"mul\((\d+),(\d+)\)";
-    let re = Regex::new(pattern1).expect("Bad regex");
+    let re1 = Regex::new(pattern1).expect("Bad regex");
 
-    let caps: Vec<(&str, &str)> = re.captures_iter(&input)
+    let caps: Vec<(i32, i32)> = re1.captures_iter(&input)
         .map(|c| {
             let (_, [x, y]) = c.extract();
-            (x, y)
+            (x.parse::<i32>().unwrap(), y.parse::<i32>().unwrap())
         }).collect();
 
-    let part1: i32 = caps.iter().map(|c| c.0.parse::<i32>().unwrap() * c.1.parse::<i32>().unwrap()).sum();
+    let part1: i32 = caps.iter().map(|c| c.0 * c.1).sum();
     print_day_result(&1, &part1);
+
+    let pattern2 = r"mul\(\d+,\d+\)|do\(\)|don\'t\(\)";
+    let re2 = Regex::new(pattern2).expect("Bad regex");
+    let matches: Vec<&str> = re2.find_iter(&input).map(|m| m.as_str()).collect();
+
+    let mut ok = true;
+    let mut part2 = 0;
+    for m in matches.iter() {
+        if m == &"do()" {
+            ok = true;
+        } else if m == &"don't()" {
+            ok = false;
+        } else if ok {
+            re1.captures(&m).map(|c| {
+                let (_, [x, y]) = c.extract();
+                part2 += x.parse::<i32>().unwrap() * y.parse::<i32>().unwrap();
+            });
+        }
+    }
+    print_day_result(&2, &part2);
 }
