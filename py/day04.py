9aec668dd8960ba6c8d8b90da772477ae31886e7
Steven Hunt <sph@commandersoft.co.uk>
2024-12-04 13:55:07 +0000

    d4 py overdiong the comprehensions

--- py/day04.py
+++ py/day04.py
@@ -1,39 +1,23 @@
+# this, perhaps, takes 'pythonic' too far
 def letter_at(x, y, grid):
     return ' ' if x < 0 or x >= len(grid[0]) or y < 0 or y >= len(grid) else grid[y][x]
 
 def find_mas(x, y, grid):
-    # pt (x,y) is an X. Now find M A S in all directions.
-    result = 0
-    for dx,dy in [(0,-1), (1,-1), (1,0), (1,1), (0,1), (-1,1), (-1,0), (-1,-1)]:
-        px = x+dx
-        py = y+dy
-        if letter_at(px, py, grid) == 'M' and letter_at(px + dx, py + dy, grid) == 'A' and letter_at(px + dx + dx, py + dy + dy, grid) == 'S':
-            result += 1
-
-    return result
+    if letter_at(x, y, grid) != 'X': return 0
+    return sum((1 if all([letter_at(x+dx*i, y+dy*i, grid) == ch for i, ch in [(1,'M'), (2,'A'), (3, 'S')]]) else 0) for dx,dy in [(0,-1), (1,-1), (1,0), (1,1), (0,1), (-1,1), (-1,0), (-1,-1)])
 
 def find_x_mas(x, y, grid):
-    # pt (x,y) is an A
-    upl = letter_at(x - 1, y - 1, grid)
-    upr = letter_at(x + 1, y - 1, grid)
-    dnl = letter_at(x - 1, y + 1, grid)
-    dnr = letter_at(x + 1, y + 1, grid)
-
-    diag1 = (upl == 'M' and dnr == 'S') or (upl == 'S' and dnr == 'M')
-    diag2 = (upr == 'M' and dnl == 'S') or (upr == 'S' and dnl == 'M')
-    return diag1 and diag2
+    if letter_at(x, y, grid) != 'A': return False
+    return all(diag in [['M', 'S'], ['S', 'M']] for diag in [ [letter_at(x - 1, y - 1, grid), letter_at(x + 1, y + 1, grid)], [letter_at(x + 1, y - 1, grid), letter_at(x - 1, y + 1, grid)] ])
 
 with open(f'../input/day04-input', 'r') as file:
     lines = [line.strip() for line in file.readlines()]
 
-part1 = 0
-part2 = 0
-for y,line in enumerate(lines):
-    for x,c in enumerate(line):
-        if c == 'X':
-            part1 += find_mas(x, y, lines)
-        elif c == 'A':
-            part2 += find_x_mas(x, y, lines)
+part1, part2 = 0, 0
+for yy,line in enumerate(lines):
+    for xx in range(0, len(line)):
+        part1 += find_mas(xx, yy, lines)
+        part2 += find_x_mas(xx, yy, lines)
 
 print("Part 1:", part1)
 print("Part 2:", part2)
