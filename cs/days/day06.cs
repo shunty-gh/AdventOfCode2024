f6dfda800b062b40e380c4027295150d7a69a8b3
Steven Hunt <sph@commandersoft.co.uk>
2024-12-06 10:02:57 +0000

    d6 cs

--- cs/days/day06.cs
+++ cs/days/day06.cs
@@ -1,72 +1,97 @@
-using System.Transactions;
-
 namespace Shunty.AoC.Days;
 
-// https://adventofcode.com/2024/day/6 -
+// https://adventofcode.com/2024/day/6 - Guard Gallivant
 
 public class Day06 : AocDaySolver
 {
     private record Point(int X, int Y);
     public int DayNumber => 6;
-    public string Title => "";
+    public string Title => "Guard Gallivant";
 
     public async Task Solve()
     {
         var input = (await File.ReadAllLinesAsync(AocUtils.FindInputFile(DayNumber))).ToImmutableList();
-        //var input = (await File.ReadAllTextAsync(AocUtils.FindInputFile(DayNumber))).Split("\n\n");
         //var input = TestData.Split('\n').ToList();
 
-        // Find ^
-        var start = new Point(-1,-1);
-        for (var y = 0; y < input.Count; y++)
-        {
-            for (var x = 0; x < input[y].Length; x++)
-            {
+        // Find start position
+        Point start = new(-1, -1);
+        for (var y = 0; y < input.Count && start.X < 0; y++)
+            for (var x = 0; x < input[y].Length && start.X < 0; x++)
                 if (input[y][x] == '^')
-                {
                     start = new(x, y);
-                    break;
-                }
-            }
-        }
 
-        int part1 = 0, part2 = 0;
-        var current = start;
-        var direction = new Point(0, -1);
-        var leave = new Point(-1, -1);
-        HashSet<Point> visited = new([start]);
-        while (true)
-        {
-            var next = new Point(current.X + direction.X, current.Y + direction.Y);
-            if (next.X < 0 || next.X >= input[0].Length || next.Y < 0 || next.Y >= input.Count)
-            {
-                // Elvis is leaving the area
-                leave = new(current.X, current.Y);
-                break;
-            }
+        var part1 = RunRoute(input, start);
+        this.ShowDayResult(1, part1);
 
-            if (input[next.Y][next.X] == '#')
-            {
-                // Turn right
-                direction = new(-direction.Y, direction.X);
-                next = new Point(current.X + direction.X, current.Y + direction.Y);
-                if (next.X < 0 || next.X >= input[0].Length || next.Y < 0 || next.Y >= input.Count)
+        int part2 = 0;
+        // Re-run the route while trying an obstacle at each possible position
+        for (var y = 0; y < input.Count; y++)
+            for (var x = 0; x < input[y].Length; x++)
+                if (input[y][x] == '.')
                 {
-                    // Elvis is leaving the area
-                    leave = new(current.X, current.Y);
-                    break;
+                    var loop = RunRoute(input, start, new Point(x, y));
+                    if (loop < 0)
+                        part2 += 1;
                 }
-            }
 
-            // else
-            visited.Add(next);
-            current = new(next.X, next.Y);
-        }
-
-        this.ShowDayResult(1, visited.Count);
         this.ShowDayResult(2, part2);
     }
 
+    /// <summary>
+    /// Run through the guards route with an optional extra obstacle at a given point (for part 2).
+    /// </summary>
+    /// <returns>The number of unique locations visited before exiting the area or -1 if we end up in a loop</returns>
+    private int RunRoute(IReadOnlyList<string> map, Point start, Point? obstacle = null)
+    {
+        const char OOB = ' '; // the character we'll use to indicate out of bounds
+        var current = start;
+        var direction = new Point(0, -1);
+        HashSet<Point> visited1 = new([start]);
+        HashSet<(Point, Point)> visited2 = new([(start, direction)]);
+
+        obstacle ??= new Point(-1, -1);
+        var p1 = obstacle.X < 0;
+        var p2 = !p1;
+        while (true)
+        {
+            var next = new Point(current.X + direction.X, current.Y + direction.Y);
+            var ch = map.CharAt(next.X, next.Y, OOB);
+            if (ch == '#' || next == obstacle)
+            {
+                // and then a step to the right
+                direction = new(-direction.Y, direction.X);
+                next = new(current.X + direction.X, current.Y + direction.Y);
+                ch = map.CharAt(next.X, next.Y, OOB);
+                if (ch == '#' || next == obstacle)
+                {
+                    // Turn right again
+                    // We won't need more than two right turns because the second turn will have
+                    // us walking back to our previous location - which we know must be ok
+                    direction = new(-direction.Y, direction.X);
+                    next = new(current.X + direction.X, current.Y + direction.Y);
+                    ch = map.CharAt(next.X, next.Y, OOB);
+                }
+            }
+            if (ch == OOB)
+            {
+                // Elvis (she/they) is leaving the area
+                break;
+            }
+
+            if (p2 && visited2.Contains((next, direction)))
+            {
+                // We've been here, going in this direction, already. Therefore it's a loop.
+                return -1;
+            }
+            // else
+            if (p1) visited1.Add(next);
+            if (p2) visited2.Add((next, direction));
+            current = new(next.X, next.Y);
+        }
+        return p1 ? visited1.Count : visited2.Count;
+    }
+
+    // Test data, expect P1 = 41; P2 = 6
     private const string TestData = """
     ....#.....
     .........#
