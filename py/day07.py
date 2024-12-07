ed1613219c510257caa98b22a031b3af04f89d05
Steven Hunt <sph@commandersoft.co.uk>
2024-12-07 10:33:44 +0000

    d7 py

--- py/day07.py
+++ py/day07.py
@@ -1,7 +1,5 @@
 from collections import deque
 
-input = open(f'../input/day07-input', 'r').read().strip().split('\n')
-
 def solveequation(target: int, nums: list[int], ispart2: bool) -> int:
     q = deque()
     q.appendleft(nums)
@@ -15,11 +13,13 @@ def solveequation(target: int, nums: list[int], ispart2: bool) -> int:
         q.appendleft([curr[0] + curr[1]] + curr[2:])
         q.appendleft([curr[0] * curr[1]] + curr[2:])
 
-        if ispart2:
+        if ispart2: # Add the concatenation of the first two numbers
             q.appendleft([int(str(curr[0]) + str(curr[1]))] + curr[2:])
 
-    return 0
+    return 0 # No solution found
 
+
+input = open(f'../input/day07-input', 'r').read().strip().split('\n')
 
 part1, part2 = 0, 0
 for line in input:
