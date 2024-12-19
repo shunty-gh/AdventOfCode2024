0ece6ccc21bb797d154a62790218e8fc50b9cb56
Steven Hunt <sph@commandersoft.co.uk>
2024-12-19 11:50:08 +0000

    d19 cs

--- cs/days/day19.cs
+++ cs/days/day19.cs
@@ -11,52 +11,70 @@ public class Day19 : AocDaySolver
     {
         var input = (await File.ReadAllLinesAsync(AocUtils.FindInputFile(DayNumber))).ToList();
         //var input = (await Task.FromResult(TestInput.Trim())).Split('\n').ToList();
-        var towels = input[0].Trim().Split(", ").ToHashSet();
+        var towels = input[0].Trim().Split(", ").AsReadOnly();
         var patterns = input[2 ..].Select(s => s.Trim());
 
-        int part1 = 0, part2 = 0;
-        foreach (var pattern in patterns)
-        {
-            part1 += CanMakePattern(towels, pattern) ? 1 : 0;
-        }
+        var part1 = patterns.Sum(p => CanMakePattern(towels, p));
         this.ShowDayResult(1, part1);
+
+        long part2 = patterns.Sum(p => PatternCombinations(towels, p, []));
         this.ShowDayResult(2, part2);
     }
 
-    private bool CanMakePattern(IReadOnlySet<string> towels, string pattern)
+    /// <summary>
+    /// This is the original fn for Part 1. Turns out not to be suitable for part 2 (surprise)
+    /// and part 2 was so much more straightforward with a bit of recursion and a cache anyway.
+    /// I could substitute the part 2 solution for this but why bother now.
+    private int CanMakePattern(IReadOnlyCollection<string> towels, string pattern)
     {
         Queue<int> q = [];
         HashSet<int> matchedlengths = [];
+        var ps = pattern.AsSpan();
         foreach (var towel in towels)
         {
-            if (pattern.StartsWith(towel))
-            {
+            if (ps.StartsWith(towel))
                 q.Enqueue(towel.Length);
-            }
         }
         var maxlen = towels.Max(t => t.Length);
-        var plen = pattern.Length;
+        var plen = ps.Length;
         while (q.Count > 0)
         {
             var len = q.Dequeue();
-            var tomatch = len+maxlen >= plen
-                ? pattern[len..].AsSpan()
-                : pattern[len..(len+maxlen)].AsSpan();
             foreach (var towel in towels)
             {
                 var newlen = len + towel.Length;
                 if (newlen > plen || matchedlengths.Contains(newlen))
                     continue;
-                if (tomatch.StartsWith(towel))
+                if (ps[len..].StartsWith(towel))
                 {
                     if (newlen == plen)
-                        return true;
-                    q.Enqueue(len + towel.Length);
+                        return 1;
+                    q.Enqueue(newlen);
                     matchedlengths.Add(newlen);
                 }
             }
         }
-        return false;
+        return 0;
+    }
+
+    private long PatternCombinations(IReadOnlyCollection<string> towels, string pattern, Dictionary<string, long> cache)
+    {
+        if (cache.TryGetValue(pattern, out var result))
+            return result;
+
+        if (string.IsNullOrWhiteSpace(pattern))
+            return 1;
+
+        result = 0;
+        foreach (var towel in towels)
+        {
+            if (pattern.StartsWith(towel))
+            {
+                result += PatternCombinations(towels, pattern[towel.Length..], cache);
+            }
+        }
+        cache[pattern] = result;
+        return result;
     }
 
     private const string TestInput = """
